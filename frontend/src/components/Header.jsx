bdc7989e676c7c886a5202cdc6ce7137c20167ff
Chris <loudowl@mac.com>
2026-05-30 14:04:12 -0700

    feat: initial implementation

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header style={{ backgroundColor: '#0047AB', color: '#FFF', padding: '10px 20px' }}>
+      <h1>Tech News Aggregator</h1>
+    </header>
+  );
+};
+
+export default Header;
